ead3bd899319324feca28dd5ebc5650d7fc17572
Hans Schallmoser <99032404+hansSchall@users.noreply.github.com>
2022-10-08 12:27:46 +0200

    set default localIP to 127.0.0.1

--- backend/src/dev.config.ts
+++ backend/src/dev.config.ts
@@ -3,5 +3,5 @@ import { ConfigOptions } from "./config";
 
 export const devConfig: ConfigOptions = {
     sacn2videoServer: path.resolve("c:/hans/ts/sacn2video/server/server.js"),
-    localIP: "10.101.111.1",
+    localIP: "127.0.0.1",
 }
