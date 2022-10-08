d2ce4be17a25ca8dc98c7e77b76086bfab1dedda
Hans Schallmoser <99032404+hansSchall@users.noreply.github.com>
2022-10-08 15:45:46 +0200

    new add-asset view (xhr upload)

--- frontend/src/ts/components/add-asset/add-asset.tsx
+++ frontend/src/ts/components/add-asset/add-asset.tsx
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { lang } from "../../lang/lang";
+import { useInput } from "../common/hook.input";
 import { Header } from "../global/header";
 
 export function AddAsset(props: { server: string }) {
-    return <>
-        <Header title={`sACN2Video Asset Editor`} />
-        <div className={"main"} style={{
-            overflow: "hidden"
-        }}>
-            <iframe className="iframe-fullscreen" src={props.server + "api/v3/ext/add-asset"}></iframe>
-        </div>
-    </>
+    const [uploading, setUploading] = useState(false);
+    const [id, updateID, setID] = useInput("");
+    const [label, updateLabel, setLabel] = useInput("");
+    const [message, setMessage] = useState("");
+    if (uploading)
+        return <>
+            <Header title={`sACN2Video Asset Editor`} />
+            <div className={"main g-dialog"}>
+                <div className="g-input-label">uploading ...</div>
+
+            </div>
+        </>
+    else
+        return <>
+            <Header title={`sACN2Video Asset Editor`} />
+            <div className={"main g-dialog"}>
+                <form /* action="/api/v3/ext/add-asset" method="post" */ encType="multipart/form-data">
+                    <div className="g-input-label">Datei</div>
+                    <input className="g-input" type="file" name="file" required />
+                    <div className="g-input-label">ID</div>
+                    <input className="g-input" type="text" name="id" placeholder="..." value={id} onChange={updateID} required />
+                    <div className="g-input-label">Label</div>
+                    <input className="g-input" type="text" name="label" placeholder="..." value={label} onChange={updateLabel} required />
+                </form>
+                {message}
+                <div className="g-dialog-button-footer">
+                    <button className="g-button gt-bt-green" tabIndex={0} onClick={() => {
+                        const files = document.querySelector<HTMLInputElement>('[name=file]')?.files
+                        const formData = new FormData()
+                        if (!(files?.[0] && id && label)) {
+                            return; // do nothing
+                        }
+                        formData.append("file", files[0]);
+                        formData.append("id", id);
+                        formData.append("label", label);
+                        const xhr = new XMLHttpRequest();
+                        xhr.addEventListener("load", () => {
+                            setUploading(false);
+                            location.reload();
+                        })
+                        xhr.open("POST", `${props.server}api/v3/ext/add-asset`);
+                        xhr.send(formData);
+                        setUploading(true);
+                    }}>Import</button>
+                    <button className="g-button gt-bt-red" tabIndex={0} onClick={() => {
+                        ipc.invoke("close-win");
+                    }}>{lang.get("cancel")}</button>
+                </div >
+            </div>
+        </>
 }
